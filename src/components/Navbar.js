05757464af565d8b375c2dd38408cb4f212d0514
Justin Swiger <justin.swiger@me.com>
2022-05-04 14:25:07 -0400

    completed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,41 @@
 import React from 'react'
-import styled from 'styled-components'
-import logo from '../assets/logo.svg'
-import { FaBars } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-import { links } from '../utils/constants'
+import styled from 'styled-components'
+import { FaBars } from 'react-icons/fa'
+
 import CartButtons from './CartButtons'
+
+import logo from '../assets/logo.svg'
+import { links } from '../utils/constants'
 import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
-  return <h4>navbar</h4>
+  return (
+    <NavContainer>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <Link to='/'>
+            <img src={logo} alt='comfy sloth logo' />
+          </Link>
+          <button type='button' className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          {links.map((link) => {
+            const { id, text, url } = link
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            )
+          })}
+        </ul>
+        <CartButtons />
+      </div>
+    </NavContainer>
+  )
 }
 
 const NavContainer = styled.nav`
