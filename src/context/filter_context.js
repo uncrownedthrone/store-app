4a80ee838ad68d1200b52abbf929dd17a42afb26
Justin Swiger <justin.swiger@me.com>
2022-05-12 12:42:38 -0400

    completed sorting products

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -17,6 +17,7 @@ const initialState = {
   filtered_products: [],
   all_products: [],
   grid_view: true,
+  sort: 'price-lowest',
 }
 
 const FilterContext = React.createContext()
@@ -29,8 +30,28 @@ export const FilterProvider = ({ children }) => {
     dispatch({ type: LOAD_PRODUCTS, payload: products })
   }, [products])
 
+  useEffect(() => {
+    dispatch({ type: SORT_PRODUCTS })
+  }, [products, state.sort])
+
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW })
+  }
+
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW })
+  }
+
+  const updateSort = (e) => {
+    // for demo
+    // const name = e.target.name
+    const value = e.target.value
+    dispatch({ type: UPDATE_SORT, payload: value })
+  }
+
   return (
-    <FilterContext.Provider value={{ ...state }}>
+    <FilterContext.Provider
+      value={{ ...state, setGridView, setListView, updateSort }}>
       {children}
     </FilterContext.Provider>
   )
