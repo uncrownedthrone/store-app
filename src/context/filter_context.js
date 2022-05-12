a53103faf7770dcaf9aadebcd7d5a744a59412e0
Justin Swiger <justin.swiger@me.com>
2022-05-12 11:05:58 -0400

    added grid, list views & set up filter context

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext, useReducer } from 'react'
-import reducer from '../reducers/filter_reducer'
+
 import {
   LOAD_PRODUCTS,
   SET_GRIDVIEW,
@@ -10,20 +10,32 @@ import {
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
 } from '../actions'
+import reducer from '../reducers/filter_reducer'
 import { useProductsContext } from './products_context'
 
-const initialState = {}
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+  grid_view: true,
+}
 
 const FilterContext = React.createContext()
 
 export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext()
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products })
+  }, [products])
+
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider value={{ ...state }}>
       {children}
     </FilterContext.Provider>
   )
 }
-// make sure use
+
 export const useFilterContext = () => {
   return useContext(FilterContext)
 }
